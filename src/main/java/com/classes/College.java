d17814b21814dbee2830677d17f7cd8198d43da1
mani <rajendiramanikandan@gmail.com>
2020-04-27 14:54:21 +0530

    Added @Component Added In xml configuration we added <context:component-scan base_package="com.classed.college"></context:component-scan>

--- src/main/java/com/classes/College.java
+++ src/main/java/com/classes/College.java
@@ -1,5 +1,8 @@
 package com.classes;
 
+import org.springframework.stereotype.Component;
+
+@Component("college")
 public class College {
 	public void show() {
 		System.out.println("Calling show method");
