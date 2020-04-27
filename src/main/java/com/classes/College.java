b6adc04aef3b6fb53fed4df1b044b9b4e706823d
mani <rajendiramanikandan@gmail.com>
2020-04-27 22:51:07 +0530

    Added @Value Annodation

--- src/main/java/com/classes/College.java
+++ src/main/java/com/classes/College.java
@@ -1,11 +1,15 @@
 package com.classes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 //@Component("college")
 public class College {
+	@Value("${College.name}")
+	String name;
 	public void show() {
 		System.out.println("Calling show method");
+		System.out.println("Colleage Name"+name);
 	}
 
 }
--- src/main/java/com/classes/ConfigurationClass.java
+++ src/main/java/com/classes/ConfigurationClass.java
@@ -1,9 +1,12 @@
 package com.classes;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-//@Configuration
+@Configuration
 //@ComponentScan(basePackages ="com.classes")
+@PropertySource("classpath:college-info.properties")
 public class ConfigurationClass {
     @Bean
 	public College college() {
