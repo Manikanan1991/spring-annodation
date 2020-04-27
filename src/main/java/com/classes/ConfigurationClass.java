0124c2428b6d07ceca5935cfe1dcdd0538c51071
mani <rajendiramanikandan@gmail.com>
2020-04-27 15:10:22 +0530

    Added ConfigurationClass @Configuration @ComponentScan(basePackage="com.classed")

---
+++ src/main/java/com/classes/ConfigurationClass.java
@@ -0,0 +1,10 @@
+package com.classes;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages ="com.classes")
+public class ConfigurationClass {
+
+}
--- src/main/java/com/sample/SpringAnnodationDemo1Application.java
+++ src/main/java/com/sample/SpringAnnodationDemo1Application.java
@@ -3,16 +3,19 @@ package com.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.classes.College;
+import com.classes.ConfigurationClass;
 
 @SpringBootApplication
 public class SpringAnnodationDemo1Application {
 
 	public static void main(String[] args) {
 		//ApplicationContext applicationContext = SpringApplication.run(SpringAnnodationDemo1Application.class, args);
-		ApplicationContext applicationContext =new ClassPathXmlApplicationContext("spring.xml");
+		//ApplicationContext applicationContext =new ClassPathXmlApplicationContext("spring.xml");
+	 ApplicationContext applicationContext= new AnnotationConfigApplicationContext(ConfigurationClass.class);
 		System.out.println("Bean is initialized");
 		College  call= (College) applicationContext.getBean(College.class);
 		call.show();
