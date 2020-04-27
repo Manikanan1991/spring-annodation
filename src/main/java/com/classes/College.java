dd9e563df67252409f19a8615c1e6f44f2b586e7
mani <rajendiramanikandan@gmail.com>
2020-04-27 14:25:54 +0530

    Bean creation using xml

---
+++ src/main/java/com/classes/College.java
@@ -0,0 +1,8 @@
+package com.classes;
+
+public class College {
+	public void show() {
+		System.out.println("Calling show method");
+	}
+
+}
--- src/main/java/com/sample/SpringAnnodationDemo1Application.java
+++ src/main/java/com/sample/SpringAnnodationDemo1Application.java
@@ -2,12 +2,23 @@ package com.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.classes.College;
 
 @SpringBootApplication
 public class SpringAnnodationDemo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAnnodationDemo1Application.class, args);
+		//ApplicationContext applicationContext = SpringApplication.run(SpringAnnodationDemo1Application.class, args);
+		ApplicationContext applicationContext =new ClassPathXmlApplicationContext("spring.xml");
+		System.out.println("Bean is initialized");
+		College  call= (College) applicationContext.getBean(College.class);
+		call.show();
+	
+		
+		
 	}
 
 }
