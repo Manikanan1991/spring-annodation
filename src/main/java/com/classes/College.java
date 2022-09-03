d169c394f3f570e0e45b551bdd893b24b67d02d7
Manikanan1991 <rajendiramanikandan@gmail.com>
2022-09-03 22:15:38 +0530

    new commit

--- src/main/java/com/classes/College.java
+++ src/main/java/com/classes/College.java
@@ -3,10 +3,11 @@ package com.classes;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-//@Component("college")
+@Component("college")
 public class College {
 	@Value("${College.name}")
 	String name;
+	String college_name="Asan College";
 	public void show() {
 		System.out.println("Calling show method");
 		System.out.println("Colleage Name"+name);
